53313515e763993dd5ac72fd14e5f1f69d26fc88
timmyke <timmyke@server>
2015-04-26 19:37:03 +0300

    ElokuvakirjastoOsa1

--- web/app/app.js
+++ web/app/app.js
@@ -1,1 +1,17 @@
-// Toteuta moduulisi tänne
+var ImApp = angular.module('ImApp', ['firebase', 'ngRoute']);
+
+
+
+ImApp.config(function ($routeProvider) {
+        $routeProvider.when('/', {
+        controller: 'MoviesController',
+        templateUrl: 'app/views/movies.html'
+    })
+            .when('/movies', {
+                controller: 'MoviesController',
+                templateUrl: 'app/views/movies.html'
+            }).when('/movies/new', {
+                controller: 'NewMovieController',
+                templateUrl: 'app/views/new_movie.html'
+            });
+});
