60a90538244f640373100eefbffce3b5029f742b
timmyke <timmyke@server>
2015-04-28 22:42:43 +0300

    muutoksia

--- web/app/controllers/api_controller.js
+++ web/app/controllers/api_controller.js
@@ -3,15 +3,20 @@ ImApp.controller('ApiController', function ($scope, APIService) {
     $scope.search = {};
     $scope.search.name = '';
     $scope.search.year = '';
+    $scope.count = -1;
 
     $scope.doSearch = function () {
-        
+        $scope.count = -1;
         APIService.findMovie($scope.search.name, $scope.search.year).success(function (movies) {
             $scope.movies = movies.Search;
             console.log(movies);
             console.log(movies.Search);
             
-            if ($scope.movies.length === 0 || )
+            if ($scope.movies.length === 0 || $scope.movies.Response === "False"  ) {
+                $scope.movies = [];
+            }
+            
+            $scope.count = $scope.movies.length;
         });
     }
 });
--- web/app/services/api_service.js
+++ web/app/services/api_service.js
@@ -1,5 +1,9 @@
-ImApp.service('APIService', function($http){
-  this.findMovie = function(name, year){
-    return $http.get('http://www.omdbapi.com', { params: { s: name, y: year } });
-  }
+ImApp.service('APIService', function ($http) {
+    this.findMovie = function (name, year) {
+        if (name === undefined)
+            return $http.get('http://www.omdbapi.com', {params: {y: year}});
+        if (year === undefined)
+            return $http.get('http://www.omdbapi.com', {params: {s: name}});
+        return $http.get('http://www.omdbapi.com', {params: {s: name, y: year}});
+    }
 });
